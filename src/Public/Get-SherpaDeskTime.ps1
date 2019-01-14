3c33ebc9a9f15b3d8d9cb58950eb4f8cad650157
Anthony Howell <anthony@howell-it.com>
2019-01-14 11:29:46 -0800

    Now gets time entries

--- src/Public/Get-SherpaDeskTime.ps1
+++ src/Public/Get-SherpaDeskTime.ps1
@@ -1,9 +1,30 @@
 Function Get-SherpaDeskTime {
+    [cmdletbinding()]
     Param(
+        [string]$Account,
+        [string]$Tech,
+        [string]$Project,
         [string]$Organization = $authConfig.WorkingOrganization,
         [string]$Instance = $authConfig.WorkingInstance,
         [string]$ApiKey = $authConfig.ApiKey
     )
+    $arrParameters = @()
+    If($AccountName){
+        $arrParameters += "account=$AccountName"
+    }
+    If($Tech){
+        $arrParameters += "tech=$Tech"
+    }
+    If($Project){
+        $arrParameters += "project=$Project"
+    }
+    $strParameters = $arrParameters -join '&'
+    
+    Write-Verbose $strParameters
 
-    Invoke-SherpaDeskAPICall -Resource time -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
+    If($strParameters){
+        Invoke-SherpaDeskAPICall -Resource "time?$strParameters" -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
+    }Else{
+        Invoke-SherpaDeskAPICall -Resource time -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
+    }
 }
