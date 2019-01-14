a1ebaf53167df87d1abb4109773360131fe1c986
Anthony Howell <anthony@howell-it.com>
2019-01-14 10:56:18 -0800

    switched locations, added some temp module management

--- build.ps1
+++ build.ps1
@@ -11,9 +11,11 @@ Foreach ($Module in $DependentModules){
     }
     Import-Module $module -ErrorAction Stop
 }
-$env:ModuleTempDir = "$PSScriptRoot\src" #$env:TEMP
+$env:ModuleTempDir = "$PSScriptRoot\build" #$env:TEMP
 $env:ModuleName = "PSSherpaDesk"
 $env:Author = "Anthony Howell"
 $env:ModuleVersion = "0.0.1"
 # Builds the module by invoking psake on the build.psake.ps1 script.
 Invoke-PSake $PSScriptRoot\psake.ps1 -taskList $Task
+remove-module pssherpadesk
+import-module .\build\PSSherpaDesk
