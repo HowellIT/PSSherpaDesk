bd8f480124825450e020bdecf704c77654f1d532
Anthony Howell <anthony@howell-it.com>
2019-01-14 11:37:24 -0800

    Now gets projects

---
+++ src/Public/Get-SherpaDeskProject.ps1
@@ -0,0 +1,18 @@
+Function Get-SherpaDeskProject {
+    [cmdletbinding()]
+    Param(
+        [parameter(
+            ParameterSetName = 'ByKey'
+        )]
+        [string]$Key,
+        [string]$Organization = $authConfig.WorkingOrganization,
+        [string]$Instance = $authConfig.WorkingInstance,
+        [string]$ApiKey = $authConfig.ApiKey
+    )
+    $resource = 'projects'
+    If($PSCmdlet.ParameterSetName -eq 'ByKey'){
+        $resource = "$resource/$key"
+    }
+
+    Invoke-SherpaDeskAPICall -Resource $resource -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
+}
