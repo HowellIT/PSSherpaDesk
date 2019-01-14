f49f80db3cd0e3ffab84cfe52fab6a3559c97393
Anthony Howell <anthony@howell-it.com>
2019-01-14 11:33:31 -0800

    now gets techs

---
+++ src/Public/Get-SherpaDeskTechs.ps1
@@ -0,0 +1,11 @@
+Function Get-SherpaDeskTechs {
+    [cmdletbinding()]
+    Param(
+        [string]$Organization = $authConfig.WorkingOrganization,
+        [string]$Instance = $authConfig.WorkingInstance,
+        [string]$ApiKey = $authConfig.ApiKey
+    )
+    $resource = 'technicians'
+
+    Invoke-SherpaDeskAPICall -Resource $resource -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
+}
