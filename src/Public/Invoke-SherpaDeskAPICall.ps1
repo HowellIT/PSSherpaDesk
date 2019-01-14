d0ed05d8c0c7e6bfad2af44bea986e12308dec1e
Anthony Howell <anthony@howell-it.com>
2019-01-14 12:35:27 -0800

    Removed unnecessary comments

--- src/Public/Invoke-SherpaDeskAPICall.ps1
+++ src/Public/Invoke-SherpaDeskAPICall.ps1
@@ -1,23 +1,3 @@
-<#
-https://github.com/sherpadesk/api/wiki
-https://documenter.getpostman.com/view/4454237/apisherpadeskcom-playground/RW8AooQg
-https://github.com/ThePoShWolf/PSSherpaDesk
-
-I do not endorsing or promoting this product and nor am I being endorsed, sponsored, or otherwise compensated to stream this product.
-
-I am streaming this because I enjoy building PS Modules to interact with APIs.
-
-# [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("")
-#>
-
-<# Goals
-Essentially streamlining my invoicing:
- - Retrieve API key from API
- - Authenticate
- - Get Tickets
- - Associate tickets to 'Accounts' (clients, departments, etc)
- - Get Time worked
-#>
 Function Invoke-SherpaDeskAPICall {
     Param(
         [string]$Resource,
