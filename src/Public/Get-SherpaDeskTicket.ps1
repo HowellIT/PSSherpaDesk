4f16de24e377a4b408bd27602d0f58d4f3260384
Anthony Howell <anthony@howell-it.com>
2019-01-14 11:02:51 -0800

    Now gets a single ticket

--- src/Public/Get-SherpaDeskTicket.ps1
+++ src/Public/Get-SherpaDeskTicket.ps1
@@ -1,9 +1,18 @@
 Function Get-SherpaDeskTicket{
+    [cmdletbinding()]
     Param(
+        [parameter(
+            ParameterSetName = 'ByKey'
+        )]
+        [string]$Key,
         [string]$Organization = $authConfig.WorkingOrganization,
         [string]$Instance = $authConfig.WorkingInstance,
         [string]$ApiKey = $authConfig.ApiKey
     )
+    $resource = 'tickets'
+    If($PSCmdlet.ParameterSetName -eq 'ByKey'){
+        $resource = "$resource/$key"
+    }
 
-    Invoke-SherpaDeskAPICall -Resource tickets -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
+    Invoke-SherpaDeskAPICall -Resource $resource -Method Get -Organization $Organization -Instance $Instance -ApiKey $ApiKey
 }
