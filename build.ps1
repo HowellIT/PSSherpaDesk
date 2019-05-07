e5d3e06522152724c21dd308c157eee135dc8c0a
Anthony Howell <anthony@howell-it.com>
2019-05-07 14:25:06 -0700

    Forcing the install to the current user

--- build.ps1
+++ build.ps1
@@ -11,7 +11,7 @@ Foreach ($Module in $DependentModules){
     Import-Module $module -ErrorAction Stop
 }
 if ((Get-Module pester -ListAvailable | Sort-Object version | Select-Object -Last 1).Version -lt 4.6){
-    Install-Module 'Pester' -MinimumVersion 4.6
+    Install-Module 'Pester' -MinimumVersion 4.6 -Scope CurrentUser -Force
 }
 Import-Module 'Pester'
 # Builds the module by invoking psake on the build.psake.ps1 script.
