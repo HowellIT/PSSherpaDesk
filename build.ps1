94925be9c2a5118855842f8f1cfe1f851930244a
Anthony Howell <anthony@howell-it.com>
2019-05-07 14:22:38 -0700

    Forcing a minimum Pester version

--- build.ps1
+++ build.ps1
@@ -3,12 +3,16 @@ param(
     [string[]]$Task = 'ModuleBuild'
 )
 
-$DependentModules = @('PSDeploy','InvokeBuild','PlatyPS','Pester') # add pester when pester tests are added
+$DependentModules = @('PSDeploy','InvokeBuild','PlatyPS') # add pester when pester tests are added
 Foreach ($Module in $DependentModules){
     If (-not (Get-Module $module -ListAvailable)){
         Install-Module -name $Module -Scope CurrentUser -Force
     }
     Import-Module $module -ErrorAction Stop
 }
+if ((Get-Module pester -ListAvailable | Sort-Object version | Select-Object -Last 1).Version -lt 4.6){
+    Install-Module 'Pester' -MinimumVersion 4.6
+}
+Import-Module 'Pester'
 # Builds the module by invoking psake on the build.psake.ps1 script.
 Invoke-Build "$PSScriptRoot\PSSherpaDesk.build.ps1" -Task $Task
